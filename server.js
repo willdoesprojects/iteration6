47b7c07f8b6109903be98e183594330067c2faa9
willdoesprojects <sunsoffire@gmail.com>
2023-11-29 20:04:04 -0500

    modified:   controllers/acctAuthController.js 	modified:   models/Users.js 	modified:   server.js 	modified:   views/pages/index.ejs

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const Preference = require("./routes/PreferencesRouter");
 const songsRoute = require("./routes/SongsRouter");
 const musicPlayerRoute = require("./routes/MusicPlayerRouter");
 
-const UserModel = require("./models/Users");
+const PlaylistModel = require("./models/playlists");
 
 const crypto = require('crypto');
 //const { db } = require("./models/Users");
@@ -347,31 +347,31 @@ const main = async() => {
 
 
 /******************************* Jason's Code ****************************/
-
-const { MongoClient } = require('mongodb');
-const uri2 = 'mongodb+srv://testuser:testpassword@cluster0.oburu6d.mongodb.net/test?retryWrites=true&w=majority';
-const client = new MongoClient(uri2);
-
 // Render the index.ejs file
-app.get('/djhomepage', (req, res) => {
+const isDJ = (req, res, next) => {
+  if (req.session.isAuth && req.session.flag == 2) {
+    next();
+  }
+
+  else {
+    res.redirect("./");
+  }
+}
+
+app.get('/djhomepage', isDJ, (req, res) => {
   res.render('index_dj');
 });
 
 // Connect to MongoDB and handle button press to retrieve song data
 app.get('/retrieveSongData', async (req, res) => {
-  try {
       // Find documents
-      const data = await findDocuments('test', 'coll_songs', {});
+      console.log(req.session.playlistId);
+      const data = await PlaylistModel.findById(req.session.playlistId);
+      console.log(data);
 
       // Send response to the client
       res.json({ success: true, message: 'Data retrieved from MongoDB', data });
-  } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-  } finally {
-      await client.close();
-      console.log('Closed MongoDB connection');
-  }
+
 });
 
 // Connect to MongoDB and handle button press to retrieve playlist data
